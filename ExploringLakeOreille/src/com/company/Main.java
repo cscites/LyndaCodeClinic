22ee92f81ef945b5fd4ee2890f42bf301e656726
Chris <cscites@gmail.com>
2017-03-02 23:21:32 -0500

    2017-03-02 (home)

--- ExploringLakeOreille/src/com/company/Main.java
+++ ExploringLakeOreille/src/com/company/Main.java
@@ -1,12 +1,15 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
 
 
@@ -19,14 +22,22 @@ public class Main {
         System.out.println("Please enter the day (1-31)");
         int day = scan.nextInt();
 
+        URL myURL = null;
         try {
-            URL myURL = new URL("https://www.weatherforyou.com/reports/index.php?config=&forecast=pass&pass=archive&pands="
-                    +Integer.toString(zipCode)+"&zipcode="+Integer.toString(zipCode)+"&place=&state=&country=us&county=&icao=" +
-                    "&hwvMetarIcao=&zone=&cwa=RNK&month="+Integer.toString(month)+"&day="+Integer.toString(day)+"&year="
-                    +Integer.toString(year));
+            myURL = new URL("https://www.weatherforyou.com/reports/index.php?config=&forecast=pass&pass=archive&pands="
+                    + Integer.toString(zipCode) + "&zipcode=" + Integer.toString(zipCode) + "&place=&state=&country=us&county=&icao=" +
+                    "&hwvMetarIcao=&zone=&cwa=RNK&month=" + Integer.toString(month) + "&day=" + Integer.toString(day) + "&year="
+                    + Integer.toString(year));
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(myURL.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
 
 
     }
